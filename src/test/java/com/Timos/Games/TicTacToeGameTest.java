10fe8fcf886de7616c596c86b75209c004882eb7
Adrian Potter <a.potter.1@elsevier.com>
2020-06-23 17:41:49 +0100

    Don't run as application test, hangs. Simplify assertion

--- src/test/java/com/Timos/Games/TicTacToeGameTest.java
+++ src/test/java/com/Timos/Games/TicTacToeGameTest.java
@@ -3,16 +3,13 @@ package com.Timos.Games;
 import com.Timos.Games.TicTacToeGame.TicTacToeGame;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class TicTacToeGameTest {
 
-    @Autowired TicTacToeGame ticTacToeGame;
+    TicTacToeGame ticTacToeGame = new TicTacToeGame();
 
     @Test
-    void shouldCreateGame(){
-        Assertions.assertFalse(ticTacToeGame == null);
+    void shouldCreateGame() {
+        Assertions.assertNotNull(ticTacToeGame);
     }
 }
