302431f1054dcf06323603d54928ee8bc7c1afb9
Adrian Potter <a.potter.1@elsevier.com>
2020-06-23 16:54:17 +0100

    Remove unused import

--- src/test/java/com/Timos/Games/ConsoleInputRetrieverTest.java
+++ src/test/java/com/Timos/Games/ConsoleInputRetrieverTest.java
@@ -3,7 +3,6 @@ package com.Timos.Games;
 import com.Timos.Games.TicTacToeGame.ConsoleInputRetriever;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
